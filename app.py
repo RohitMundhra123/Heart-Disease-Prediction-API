f7ca2e9bce44f94e64a197fe02c3c78504293e7e
RohitMundhra123 <rohitm1924@gmail.com>
2023-04-15 15:13:18 +0530

    change

--- app.py
+++ app.py
@@ -14,21 +14,21 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    gender = int(request.form.get('gender'))
-    age = int(request.form.get('age'))
-    hypertension = int(request.form.get('hypertension'))
-    ever_married = int(request.form.get('marital'))
-    work_type = int(request.form.get('work'))
-    residence_type = int(request.form.get('residence'))
-    avg_glucose_level = int(request.form.get('glucose'))
-    bmi = int(request.form.get('bmi'))
-    smoking_status = int(request.form.get('smoking'))
+    gender = float(request.form.get('gender'))
+    age = float(request.form.get('age'))
+    hypertension = float(request.form.get('hypertension'))
+    ever_married = float(request.form.get('marital'))
+    work_type = float(request.form.get('work'))
+    residence_type = float(request.form.get('residence'))
+    avg_glucose_level = float(request.form.get('glucose'))
+    bmi = float(request.form.get('bmi'))
+    smoking_status = float(request.form.get('smoking'))
 
-    input_query = np.array([gender,age,hypertension,ever_married,work_type,residence_type,avg_glucose_level,bmi,smoking_status]).reshape(1, -1)
+    input_query = np.array([gender,age,hypertension,ever_married,work_type,residence_type,avg_glucose_level*4.76,bmi,smoking_status]).reshape(1, -1)
     result = model.predict(input_query)[0]
     heart_disease = int(result)
 
-    input_query2 = np.array([gender, age, hypertension,heart_disease, ever_married, work_type, residence_type, avg_glucose_level, bmi,smoking_status]).reshape(1, -1)
+    input_query2 = np.array([gender, age, hypertension,heart_disease, ever_married, work_type, residence_type, avg_glucose_level*4.76, bmi,smoking_status]).reshape(1, -1)
     result2 = model1.predict(input_query2)[0]
 
     return jsonify({'heart disease':str(result)},{'heart n stroke':str(result2)})
