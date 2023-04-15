08768422e73d6d606ac4eadb3178d0ffbc67d3e2
RohitMundhra123 <rohitm1924@gmail.com>
2023-04-15 22:54:42 +0530

    change

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 
 
 model = pickle.load(open('heart_disease_model.pkl', 'rb'))
-model1 = pickle.load(open('stroke_model.pkl','rb'))
 
 app = Flask(__name__)
 
@@ -17,21 +16,17 @@ def predict():
     gender = float(request.form.get('gender'))
     age = float(request.form.get('age'))
     hypertension = float(request.form.get('hypertension'))
-    ever_married = float(request.form.get('marital'))
-    work_type = float(request.form.get('work'))
-    residence_type = float(request.form.get('residence'))
-    avg_glucose_level = float(request.form.get('glucose'))
+    marital = float(request.form.get('marital'))
+    work = float(request.form.get('work'))
+    residence = float(request.form.get('residence'))
+    glucose = float(request.form.get('glucose'))
     bmi = float(request.form.get('bmi'))
-    smoking_status = float(request.form.get('smoking'))
+    smoking = float(request.form.get('smoking'))
 
-    input_query = np.array([gender,age,hypertension,ever_married,work_type,residence_type,avg_glucose_level*4.76,bmi,smoking_status]).reshape(1, -1)
+    input_query = np.array([gender,age,hypertension,marital,work,residence,glucose*4.76,bmi,smoking]).reshape(1, -1)
     result = model.predict(input_query)[0]
-    heart_disease = int(result)
 
-    input_query2 = np.array([gender, age, hypertension,heart_disease, ever_married, work_type, residence_type, avg_glucose_level*4.76, bmi,smoking_status]).reshape(1, -1)
-    result2 = model1.predict(input_query2)[0]
-
-    return jsonify({'heart disease':str(result)},{'heart n stroke':str(result2)})
+    return jsonify({'heart disease':str(result)})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000)
